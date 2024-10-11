19cffb1be9aeb49557f8b74303d4c9ef6a260032
Daniel Rueda <daniesteban1006@gmail.com>
2024-10-11 17:05:59 -0500

    Migre la home page a React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from './components/head';
+import HomeBody from './components/homeBody';
+import './Styles.css';
+
+function App() {
+  return (
+    <>
+      <Head />
+      <HomeBody />
+    </>
+  );
+}
+
+export default App;
